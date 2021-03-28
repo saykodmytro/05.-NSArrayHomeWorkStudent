f245628c99c36613ab0e24fa06204eaa0b68236e
Dmytro Sayko <saykodmytro@icloud.com>
2021-03-28 21:13:40 +0300

    Initial Commit

---
+++ 05. NSArrayHomeWorkStudent/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  05. NSArrayHomeWorkStudent
+//
+//  Created by Dmytro Sayko on 28.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 05. NSArrayHomeWorkStudent/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  05. NSArrayHomeWorkStudent
+//
+//  Created by Dmytro Sayko on 28.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
